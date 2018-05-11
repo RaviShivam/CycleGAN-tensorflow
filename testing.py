a928fd3fafc88132a1c9fa2fa1585d110a85fd74
Ravi Shivam Autar <raviautar27@gmail.com>
2018-05-11 19:12:05 +0200

    Add tensor operation for cropping and refitting of image

--- testing.py
+++ testing.py
@@ -3,18 +3,15 @@ from PIL import Image
 from matplotlib import pyplot as plt
 import tensorflow as tf
 
-im = np.array(Image.open("test.jpg"))
-imtf = tf.constant(im, dtype=tf.float32)
-bbox = tf.constant([140, 0, 50, 100, 100, 100], dtype=tf.int32)
+a = tf.ones([3, 3])
+paddings = tf.constant([[4,1], [2,2]])
+b = tf.pad(a, paddings)
 
-newim = tf.py_func(crop_and_resize_real, [imtf, bbox], tf.float32)
-newim = tf.zeros_like(newim)
-finalim = tf.py_func(resize_and_refit_fake, [imtf, newim, bbox], tf.float32)
+c = tf.shape(b)
 
+x, y = [c[0], c[1]]
 
-var = np.random.random((1, 10, 10, 10))
-a = var[0, :, :, :]
-b = np.reshape(a, (1, 10, 10, 10))
-# with tf.Session() as sess:
-#     plt.imshow(sess.run(finalim))
-#     plt.show()
+with tf.Session() as sess:
+    print(sess.run(c))
+    print(sess.run(x))
+
